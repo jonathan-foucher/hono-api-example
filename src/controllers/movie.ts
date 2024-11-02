63845af9895c5f6789e37c20d8654e9eb85c38f0
Jonathan Foucher <jonathan-foucher@outlook.fr>
2024-11-02 22:17:01 +0100

    add db connection

--- src/controllers/movie.ts
+++ src/controllers/movie.ts
@@ -1,22 +1,28 @@
 import { Hono } from 'hono'
 import * as logger from '../utils/logger.ts'
+import * as dbConnection from '../db-connection.ts'
+import camelcaseKeys from 'camelcase-keys'
+import snakecaseKeys from 'snakecase-keys'
 import { type Movie } from '../models/movie.ts'
 
 const app = new Hono()
 
-app.get((c) => {
+app.get(async (c) => {
     logger.info('Get all movies')
-    return c.text('Get all movies')
+    return dbConnection.getAllMovies()
+      .then(movies => c.json(snakecaseKeys(movies)))
   })
   .post(async (c) => {
-    const movie: Movie = await c.req.json()
+    const movie: Movie = camelcaseKeys(await c.req.json())
     logger.info(`Post movie with id ${movie.id}`)
-    return c.text(`Post movie with id ${movie.id}`)
+    return dbConnection.saveMovie(movie)
+      .then(() => c.body(null))
   })
-  .delete('/:movieId', (c) => {
+  .delete('/:movieId', async (c) => {
     const movieId: number = parseInt(c.req.param("movieId"), 10)
     logger.info(`Delete movie with id ${movieId}`)
-    return c.text(`Delete movie with id ${movieId}`)
+    return dbConnection.deleteMoveById(movieId)
+      .then(() => c.body(null))
   })
 
 export default app
--- src/models/movie.ts
+++ src/models/movie.ts
@@ -1,5 +1,23 @@
+import { Table, Model, Column, PrimaryKey, AllowNull, DataType } from 'sequelize-typescript'
+
 export interface Movie {
   id: number
   title: string
   releaseDate: Date
+}
+
+@Table({ tableName: 'movie', timestamps: false })
+export class MovieModel extends Model<Movie, Movie> {
+  @PrimaryKey
+  @AllowNull(false)
+  @Column(DataType.INTEGER)
+  declare id: number
+
+  @AllowNull(false)
+  @Column(DataType.STRING)
+  declare title: string
+
+  @AllowNull(false)
+  @Column({ field: 'release_date', type: DataType.DATEONLY })
+  declare releaseDate: Date
 }
